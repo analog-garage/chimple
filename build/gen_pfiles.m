b1dbc7f9a2e951345531b0ee8a7ac4dfe814fbc3
Shawn Hershey <shawn.hershey@analog.com>
2013-08-21 15:33:41 -0400

    updated doc

--- build/gen_pfiles.m
+++ build/gen_pfiles.m
@@ -1,3 +1,3 @@
-pcode Chimple_v0_01/demo/1_basic/myprogram.m -inplace;
-pcode Chimple_v0_01/demo/1_basic/run.m -inplace;
+pcode chimple_v0_01/demo/1_basic/myprogram.m -inplace;
+pcode chimple_v0_01/demo/1_basic/run.m -inplace;
 quit;
